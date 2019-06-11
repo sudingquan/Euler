a2f0e0f3183e04808dd883891fd81f889e080934
sudingquan <1151015256@qq.com>
2019-06-11 15:26:25 +0800

    modify EP28.cpp

--- EP28.cpp
+++ EP28.cpp
@@ -12,8 +12,13 @@ using namespace std;
 int main() {
     int sum = 1;
     //推导出通项公式
+    //奇数项平方和通项公式为n(2n+1)(2n-1)/3
+    //通项公式(4n^3+3n^2+8n-9)/6
     for (int l = 3; l <= 1001; l += 2) {
         sum += 4 * l * l - 6 * l + 6;
     }
+    long long n = 1001;
+    long long sum2 = (4 * n * n * n + 3 * n * n + 8 * n - 9) / 6;
+    printf("%lld\n", sum2);
     printf("%d\n", sum);
 }
