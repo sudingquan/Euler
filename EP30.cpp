9a7e2f77afa2860a6ca57c038334530a63f825d1
sudingquan <1151015256@qq.com>
2019-06-11 15:43:31 +0800

    modify EP30.cpp

--- EP30.cpp
+++ EP30.cpp
@@ -6,6 +6,7 @@
  ************************************************************************/
 
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
@@ -14,5 +15,19 @@ using namespace std;
 int is_valid(int x) {
     int raw = x, temp = 0;
     while (x) {
+        temp += pow(x % 10, 5);
+        x /= 10;
     }
+    return temp == raw;
+}
+
+int main() {
+    int sum = 0;
+    for (int i = 2; i <= MAX_N; i++) {
+        if (!is_valid(i)) continue;
+        sum += i;
+        printf("i : %d\n", i);
+    }
+    printf("%d\n", sum);
+    return 0;
 }
