f40dc4c3b68cb5bfcadaf0818a2d679cd0530ec4
sudingquan <1151015256@qq.com>
2019-06-16 02:39:43 +0800

    EP10.cpp

---
+++ EP10.cpp
@@ -0,0 +1,26 @@
+/*************************************************************************
+	> File Name: EP10.cpp
+	> Author: sudingquan
+	> Mail: 1151015256@qq.com
+	> Created Time: 六  6/15 18:24:21 2019
+ ************************************************************************/
+
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <map>
+#include <cmath>
+using namespace std;
+#define MAX_N 2000000
+
+int is_prime[MAX_N + 5];
+int prime[MAX_N + 5];
+
+int main() {
+
+    return 0;
+}
+
