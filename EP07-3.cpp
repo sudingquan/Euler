40866be4185371d16270f4aa5765cb3bdde9da94
sudingquan <1151015256@qq.com>
2019-06-26 13:48:19 +0800

    6-25

--- EP07-3.cpp
+++ EP07-3.cpp
@@ -23,7 +23,7 @@ int main() {
         if (is_prime[M] == 0) {
             prime[++prime[0]] = M;
         }
-        for(int j = 1; j<= prime[0]; j++) {
+        for(int j = 1; j <= prime[0]; j++) {
             if (prime[j] * M > MAX_N) break;
             is_prime[prime[j] * M] = 1;
             if (M % prime[j] == 0) break;
